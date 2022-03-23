d52238756206a6ba194c16fe0a8e6bf5c36d2636
jaehyeokGil <81075170+wogur2689@users.noreply.github.com>
2022-03-24 02:33:22 +0900

    22.03.24 #6-2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
+//state가 변화할 때 모든 컴포넌트는 다시 실행되고 api호출은 처음 변화할때 한번만 하고싶은경우 해결방법은?
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev +1);
-  console.log("call an api"); //state가 변화할 때 모든 컴포넌트는 다시 실행되고 api호출은 처음 변화할때 한번만 하고싶은경우 해결방법은?
+  console.log("i run all the time"); 
+  const iRunOnlyOnce = () => {
+    console.log("i run only once")
+  }
+  useEffect(iRunOnlyOnce, []); //
+  useEffect(() => {
+    console.log("CALL THE API....");
+  }, []); //useEffect는 코드가 딱 한번만 실행될 수 있게 보호함.
   return (
     <div>
       <h1 className={styles.title}>Welcome back!</h1>
