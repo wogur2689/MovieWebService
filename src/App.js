fe191dd6c12a22a05392616c77262a320cd8cabc
jaehyeokGil <81075170+wogur2689@users.noreply.github.com>
2022-03-28 01:02:54 +0900

    22.03.27 #6.4

--- src/App.js
+++ src/App.js
@@ -2,10 +2,35 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
+function Hello() {
+  function destroyedFn() {
+    console.log(" bye :("); //컴포넌트 삭제시 출력
+  }
+  function hiFn() {
+    console.log(" hi :)"); //컴포넌트 생성시 출력
+    return byFn;
+  }
+  useEffect(hiFn, []);
+
+
+  useEffect(() => {
+    console.log("I'm here!"); //컴포넌트가 생성과 삭제가 진행되기 때문에 함수가 실행될때마다 useEffect를 해도 콘솔이 출력됨.
+  },[]);
+  useEffect(() => {
+    console.log("created :)"); //컴포넌트 생성시 출력
+    return () => console.log("destoryed :("); //컴포넌트 삭제시 출력
+  },[]);
+  return (<h1>Hello</h1>);
+}
+
 //state가 변화할 때 모든 컴포넌트는 다시 실행되고 api호출은 처음 변화할때 한번만 하고싶은경우 해결방법은?
 function App() {
+  const [showing, setShowing] = useState(false);
   const [counter, setValue] = useState(0); //버튼 클릭횟수
   const [keyword, setKeyword] = useState(""); //검색 내용
+
+  const click = () => setShowing((prev) => !prev);
+
   const onClick = () => setValue((prev) => prev +1);
   const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time"); 
@@ -35,6 +60,10 @@ function App() {
       <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
+      <hr></hr>
+      {/*버튼을 클릭하면 보여줌.*/}
+      {showing ? <Hello/> : null}
+      <button onClick={click}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
