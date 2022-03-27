b38458570165722e7cdf6a9d6600b2dcd6b50853
jaehyeokGil <81075170+wogur2689@users.noreply.github.com>
2022-03-27 23:53:15 +0900

    주석 추가

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { useState, useEffect } from "react";
 
 //state가 변화할 때 모든 컴포넌트는 다시 실행되고 api호출은 처음 변화할때 한번만 하고싶은경우 해결방법은?
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
+  const [counter, setValue] = useState(0); //버튼 클릭횟수
+  const [keyword, setKeyword] = useState(""); //검색 내용
   const onClick = () => setValue((prev) => prev +1);
   const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time"); 
