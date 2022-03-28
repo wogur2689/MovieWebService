e4714dba52e2eea6863fb667452b4c388252a751
jaehyeokGil <81075170+wogur2689@users.noreply.github.com>
2022-03-29 00:34:10 +0900

    22.03.09 #7.1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import { useState, useEffect } from "react";
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(toDo);
+    if(todo === "") {
+      return;
+    }
+    setToDo(""); //state는 직접적으로 수정 X
+    setToDos((currentArray) => [toDo, ...currentArray]) //새로운 배열 리턴
+  }
+
   return (
-    <div></div>
+    <div> 
+      <h1>My To Dos ({toDos.length})/</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
+        <button>Add To Do</button>
+      </form>
+    </div>
   );
 }
 
