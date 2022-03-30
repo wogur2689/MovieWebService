da89a2cf27db92d6c88fe0137430d9a82fc585dc
jaehyeokGil <81075170+wogur2689@users.noreply.github.com>
2022-03-30 23:16:04 +0900

    re init

--- src/App.js
+++ src/App.js
@@ -1,33 +1,6 @@
-import { useState } from "react";
-
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    console.log(toDo);
-    if(toDo === "") {
-      return;
-    }
-    setToDo(""); //state는 직접적으로 수정 X
-    setToDos((currentArray) => [toDo, ...currentArray]) //새로운 배열 리턴
-  }
-
   return (
-    <div> 
-      <h1>My To Dos ({toDos.length})/</h1>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => ( //Map를 이용한 li추가
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
+    <div></div>
   );
 }
 
